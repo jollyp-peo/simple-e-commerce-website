8aaca6e46ff53307358928fba3506a8a4c6ee35a
Jollyp <erubizzltd@gmail.com>
2025-03-30 15:18:50 +0100

    Restructure the files

---
+++ data/product.js
@@ -0,0 +1,24 @@
+export const products = [
+  { id: 1, name: "Chronograph Watch", img: "../images/wrist-watch.jpg", price: 34.99, rating: 4 },
+  { id: 2, name: "White Airpods", img: "../images/airpods.jpg", price: 199.99, rating: 5 },
+  { id: 3, name: "Flap Sling Bag", img: "../images/whitebag.jpg", price: 79.99, rating: 5 },
+  { id: 4, name: "VR Headset", img: "../images/vr-headset.jpg", price: 149.99, rating: 4 },
+  { id: 5, name: "Smartphone", img: "../images/smartphone.jpg", price: 999.98, rating: 5 },
+  {
+    id: 6, name: "Wireless Headphones", img: "../images/headset.jpg", price: 299.99, rating: 4
+  },
+  { id: 7, name: "T-shirt", img: "../images/t-shirt.jpg", price: 399.99, rating: 5 },
+  { id: 8, name: "Power Bank", img: "../images/powerbank.jpg", price: 49.35, rating: 4 },
+  { id: 9, name: "Tablet", img: "../images/tablet.jpg", price: 499.25, rating: 5 },
+  { id: 10, name: "Apple Laptop", img: "../images/laptop.jpg", price: 999.27, rating: 4 },
+  {id: 11, name: "Gaming Console", img: "../images/gaming-console.jpg", price: 799.99, rating: 5},
+  {id: 12, name: "Gaming Chair", img: "../images/gaming-chair.jpg", price: 210.20, rating: 4},
+  {id: 13, name: "Gaming Keyboard", img: "../images/gaming-kryboard.jpg", price: 110.99, rating: 5},
+  {id: 14, name: "Gaming Mouse", img: "../images/gsming-mouse.jpg", price: 28.88, rating: 4},
+  {id: 15, name: "Modern fridge", img: "../images/fridge.jpg", price: 123.89, rating: 5},
+  {id: 16, name: "Kettle", img: "../images/kettle.jpg", price: 23.89, rating: 5},
+  {id: 17, name: "52 Inches TV", img: "../images/tv.jpg", price: 70.10, rating: 4},
+  {id: 18, name: "Cctv camera", img: "../images/cctv.jpg", price: 18.10, rating: 5},
+  {id: 19, name: "Sofa", img: "../images/sofa.jpg", price: 60.10, rating: 4},
+  {id: 20, name: "Microwave", img: "../images/microwave.jpg", price: 45.10, rating: 5},
+];
